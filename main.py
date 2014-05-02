295b4da4ab7003f56f0b75ab4618825b458d097d
Malea Grubb <maleangrubb@gmail.com>
2014-05-02 19:55:36 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import re
+from sys import argv
+import math
+
+def main(program):
+    
+
+
+if __name__=='__main__':
+    # confirm that program is supplied with a file to analyze
+    if (len(argv) != 2):
+        print('No root directory supplied! Please supply a directory path!')
+    else:
+        program = argv[1]
+        main(program)
