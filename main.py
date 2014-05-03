cd5817b7e831440617ed6cfe761fc75171a91dab
Malea Grubb <maleangrubb@gmail.com>
2014-05-02 20:15:47 -0400

    Clean up code

--- main.py
+++ main.py
@@ -3,18 +3,10 @@ from sys import argv
 import math
 
 def allLines(program):
-    fstr = open(filename)                  
-    lines = 0
-    buf_size = 1024 * 1024
-    read_f = f.read # loop optimization
-    buf = read_f(buf_size)
-    while buf:
-        lines += buf.count('\n')
-        buf = read_f(buf_size)
-    return lines
+    return sum(1 for line in open(program))
 
 def allComments(program):
-    fstr = open(path).read()
+    fstr = open(program).read()
     return re.subn('(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|(//.*)',"",fstr)[1]
     
 def percentage(part,whole):
@@ -22,13 +14,11 @@ def percentage(part,whole):
     
 def main(program):
     total = allLines(program)
-    comments = allComments
+    comments = allComments(program)
     print(percentage(comments,total))
 
 if __name__=='__main__':
-    # confirm that program is supplied with a file to analyze
     if (len(argv) != 2):
         print('No root directory supplied! Please supply a directory path!')
-    else:
-        program = argv[1]
-        main(program)
+    program = argv[1]
+    main(program)
