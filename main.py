3eec7617075e54c4e7089f31f91a1047705ab34b
Malea Grubb <maleangrubb@gmail.com>
2014-05-02 20:05:50 -0400

    Add reg ex portion

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-import re
+from re import subn
 from sys import argv
 import math
 
-def main(program):
-    
 
+def allLines(program):
+    fstr = open(filename)                  
+    lines = 0
+    buf_size = 1024 * 1024
+    read_f = f.read # loop optimization
+    buf = read_f(buf_size)
+    while buf:
+        lines += buf.count('\n')
+        buf = read_f(buf_size)
+    return lines
+
+def allComments(program):
+    fstr = open(path).read()
+    return re.subn('(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|(//.*)',"",fstr)[1]
+    
+def percentage(part,whole):
+    return 100 * float(part)/float(whole)
+    
+def main(program):
+    total = allLines(program)
+    comments = allComments
+    print(percentage(comments,total))
 
 if __name__=='__main__':
     # confirm that program is supplied with a file to analyze
