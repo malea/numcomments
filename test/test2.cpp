b287a90290065214adaacdcdfa5fa64ba0d9fedd
Malea Grubb <maleangrubb@gmail.com>
2014-05-02 20:22:05 -0400

    Add additional test case for multiline comments

---
+++ test/test2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+/* This is our main function */
+int main() {
+    // print Hello world!
+    std::cout << "HELLO WORLD!" << std::endl;
+    return 1;
+    /* TESTING
+    MULTILINE
+    COMMENTS
+    YAY */
+}
